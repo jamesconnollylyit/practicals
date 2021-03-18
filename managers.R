21b8258c259d9299022f63b71c221e089b50195d
Connolly James <james.connolly@lyit.ie>
2021-03-18 11:01:16 +0000

    Commit code based on week 2 requirements

--- managers.R
+++ managers.R
@@ -37,4 +37,69 @@ colnames(managers_data) <- column_names
 
 managers_data
 
+# Recode the incorrect 'age' to NA
+managers_data$Age[managers_data$Age == 99] <- NA
+managers_data
+
+# 2 options to create a new variable
+# 1 - create a new vector and store the logical check in it
+# 2 - create the new var when doing the logical check
+
+managers_data$AgeCat[managers_data$Age >= 45] <- "Elder"
+managers_data$AgeCat[managers_data$Age >= 26 & managers_data$Age <= 44] <- "Middle Aged"
+managers_data$AgeCat[managers_data$Age <= 25] <- "Young"
+managers_data$AgeCat[is.na(managers_data$Age)] <- "Elder"
+managers_data
+
+# recode age_cat so that it is ordinal and factored
+# with the order young, middle aged, elder
+age_cat <- factor(managers_data$AgeCat, order = TRUE, levels = c("Young", "Middle Aged", "Elder"))
+age_cat
+
+# replace mnager_data age_cat variable with
+# the factored variable
+
+managers_data$AgeCat <-age_cat
+managers_data
+
+# Look at the structure of the data frame
+str(managers_data)
+
+managers_data
+
+# Create a new col called 'summary_col' that
+# contains a summary of each row
+summary_col <- managers_data$Q1 + 
+  managers_data$Q2 + 
+  managers_data$Q3 + 
+  managers_data$Q4 + 
+  managers_data$Q5
+
+summary_col
+
+# Add summary_col to the end of the data frame
+# take content of data frame, create a new data frame
+# and add sumary_col vector to the data frame
+
+managers_data <- data.frame(managers_data, summary_col)
+managers_data
+
+# Calculate mean value for each row
+mean_value <- rowMeans(managers_data[5:9])
+mean_value
+
+# Add the mean_value vector to the data frame
+
+managers_data <- data.frame(managers_data, mean_value)
+managers_data
+
+# Change variable titles to make them more readable
+names(managers_data)[11] <- "Summary"
+names(managers_data)[12] <- "Mean value"
+
+# show the structure of the data frame
+str(managers_data)
+managers_data
+
+
 
