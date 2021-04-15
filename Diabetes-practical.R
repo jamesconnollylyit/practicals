0d07572fa2d8476145f084e3528b1adae92c8b7e
Connolly James <james.connolly@lyit.ie>
2021-04-15 12:08:59 +0100

    Commit changes with charts

--- Diabetes-practical.R
+++ Diabetes-practical.R
@@ -21,9 +21,67 @@ missing_values <- aggr(diabetes_data, prop = FALSE, numbers = TRUE)
 # Show content of missing values
 summary(missing_values)
 
+# Dealing with missing data
+# I've decided to keep only data with 
+# missing type and health status
+# And the addresses are not important
+keep_this_data <- diabetes_data[!complete.cases(diabetes_data$Daibetes.type, diabetes_data$Status),]
+keep_this_data
+dim(keep_this_data)
+# 15 rows with missing data that I'd like to keep
+# Reverse the logic to contain relevant data
+keep_this_data <- diabetes_data[complete.cases(diabetes_data$Daibetes.type, diabetes_data$Status),]
+keep_this_data
+
+dim(keep_this_data)
+dim(diabetes_data)
+
+# Replacw the original data frame with the content of the processed data
+diabetes_data <- keep_this_data
+head(diabetes_data, 15)
 
 
+# 4- Configure Type to an unordered factor with 2 levels
+diabetes_data$Type <- factor(diabetes_data$Type, order = FALSE, levels = c("Type 1", "Type 2"))
 
+# Refactor status too
+diabetes_data$Status <- factor(diabetes_data$Status, order = TRUE, levels = c("Poor", "Improved", "Excellent"))
+
+#5 - Define the new column names
+col_names <- c("Patient Name", "NI address", "Type", "Age", "Health status")
+colnames(diabetes_data) <- col_names
+
+str(diabetes_data)
+
+# lets look at the class of each element inthe dta frame
+class_list <- lapply(diabetes_data, class)
+class_list
+
+class_list <- sapply(diabetes_data, class)
+class_list
+
+# Charts -----------------------------------------------------
+
+
+# Build the data frame first
+dose <- c(20, 30, 40, 45, 60)
+drug_a <- c(16, 20, 27, 40, 60)
+drug_b <- c(15, 18, 25, 31, 40)
+  
+# Create a new data frame
+drugs <- data.frame(dose, drug_a, drug_b)
+str(drugs)
+class(drugs)
+
+# general plot of data
+plot(drugs)
+
+attach(drugs)
+plot(dose, type = "o", col = "blue")
+?plot
+
+# option type = "b" shows both lines should be plotted
+plot(dose, drug_a, type = "b")
 
 
 
